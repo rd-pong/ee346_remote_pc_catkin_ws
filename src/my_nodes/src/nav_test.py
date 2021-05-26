861b61dd171ee8473eba4afd78daca80becf07eb
readypeng <38313898+readypeng@users.noreply.github.com>
2021-05-26 15:15:01 +0800

    add audio

--- src/my_nodes/src/nav_test.py
+++ src/my_nodes/src/nav_test.py
@@ -29,9 +29,12 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from random import sample
 from math import pow, sqrt
 from collections import OrderedDict
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
 
 class NavTest():
     def __init__(self):
+        soundhandle = SoundClient()
         rospy.init_node('nav_test', anonymous=True)
         
         rospy.on_shutdown(self.shutdown)
@@ -57,10 +60,10 @@ class NavTest():
 
         # ---------------4 POINTS-----------------------------------------------------------
         # Locations for bigger simulation map
-        # locations['PIT STOP 1'] = Pose(Point(1.6, -2.5, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
-        # locations['PIT STOP 2'] = Pose(Point(1.7, 2.5, 0.000), Quaternion(0.000, 0.000, 0.223, 0.975))
-        # locations['PIT STOP 3'] = Pose(Point(-3.3, 2.5, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
-        # locations['PIT STOP 4'] = Pose(Point(-3.5, -2.6, 0.000), Quaternion(0.000, 0.000, 0.786, 0.618))
+        locations['PIT STOP 1'] = Pose(Point(1.6, -2.5, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
+        locations['PIT STOP 2'] = Pose(Point(1.7, 2.5, 0.000), Quaternion(0.000, 0.000, 0.223, 0.975))
+        locations['PIT STOP 3'] = Pose(Point(-3.3, 2.5, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
+        locations['PIT STOP 4'] = Pose(Point(-3.5, -2.6, 0.000), Quaternion(0.000, 0.000, 0.786, 0.618))
         
         # # Locations for real lab environment (low precision) real1
         # locations['PIT STOP 1'] = Pose(Point(0.5, 1, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
@@ -72,15 +75,15 @@ class NavTest():
         # locations['SCENIC SPOT 2'] = Pose(Point(4.69, 4.14, 0.000), Quaternion(0.000, 0.000, 0.99, 0))
         # locations['PIT STOP 4'] = Pose(Point(0.47, 5.05, 0.000), Quaternion(0.000, 0.000, 0.75, -3))
     
-        # Locations for real lab environment (HI precision) real4
-        # locations['PIT STOP 1'] = Pose(Point(0.359, -0.370, 0.000), Quaternion(0.000, 0.000, -0.001, 0.99))
-        # locations['PIT STOP 1.5'] = Pose(Point(1.080, 0.677, 0.000), Quaternion(0.000, 0.000, -0.015, 0.9998))
-        locations['PIT STOP 2'] = Pose(Point(4.144, -0.175, 0.000), Quaternion(0.000, 0.000, 0.9998, 0.016))
-        locations['SCENIC SPOT 0.5'] = Pose(Point(2.730, -0.102, 0.000), Quaternion(0.000, 0.000, 0.999, 0.008))
-        locations['SCENIC SPOT 1'] = Pose(Point(2.847, -0.081, 0.000), Quaternion(0.000, 0.000, 0.9999,  0.027))
-        locations['PIT STOP 3'] = Pose(Point(4.34, 3.960, 0.000), Quaternion(0.000, 0.000, -0.7271, 0.686))
-        locations['SCENIC SPOT 2'] = Pose(Point(4.294,  3.0922, 0.000), Quaternion(0.000, 0.000, 0.9987, -0.049))
-        locations['PIT STOP 4'] = Pose(Point(0, 4.2, 0.000), Quaternion(0.000, 0.000, -0.031, 0.999))
+        # # Locations for real lab environment (HI precision) real4
+        # # locations['PIT STOP 1'] = Pose(Point(0.359, -0.370, 0.000), Quaternion(0.000, 0.000, -0.001, 0.99))
+        # # locations['PIT STOP 1.5'] = Pose(Point(1.080, 0.677, 0.000), Quaternion(0.000, 0.000, -0.015, 0.9998))
+        # locations['PIT STOP 2'] = Pose(Point(4.144, -0.175, 0.000), Quaternion(0.000, 0.000, 0.9998, 0.016))
+        # locations['SCENIC SPOT 0.5'] = Pose(Point(2.730, -0.102, 0.000), Quaternion(0.000, 0.000, 0.999, 0.008))
+        # locations['SCENIC SPOT 1'] = Pose(Point(2.847, -0.081, 0.000), Quaternion(0.000, 0.000, 0.9999,  0.027))
+        # locations['PIT STOP 3'] = Pose(Point(4.34, 3.960, 0.000), Quaternion(0.000, 0.000, -0.7271, 0.686))
+        # locations['SCENIC SPOT 2'] = Pose(Point(4.294,  3.0922, 0.000), Quaternion(0.000, 0.000, 0.9987, -0.049))
+        # locations['PIT STOP 4'] = Pose(Point(0, 4.2, 0.000), Quaternion(0.000, 0.000, -0.031, 0.999))
         # --------------------------------------------------------------------------
 
         # Publisher to manually control the robot (e.g. to stop it)
@@ -189,6 +192,7 @@ class NavTest():
                 state = self.move_base.get_state()
                 if state == GoalStatus.SUCCEEDED:
                     rospy.loginfo("Goal succeeded!")
+                    
                     n_successes += 1
                     distance_traveled += distance
                     rospy.loginfo("State:" + str(state))
@@ -205,6 +209,7 @@ class NavTest():
                           str(100 * n_successes/n_goals) + "%")
             rospy.loginfo("Running time: " + str(trunc(running_time, 1)) + 
                           " min Distance: " + str(trunc(distance_traveled, 1)) + " m")
+            soundhandle.playWave('stop.m4a',self.rest_time)
             rospy.sleep(self.rest_time)
             
     def update_initial_pose(self, initial_pose):
